518acbd2a797eb6e9940bcd3e756b0dc5b37c6ae
Chris Christensen <Chriscrossed.86@gmail.com>
2018-12-04 23:21:22 -0800

    Main - Player Controller - Main Creation
    - Creating player input

---
+++ Trillium/Assets/cs_PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cs_PlayerController : MonoBehaviour
+{
+    private GameObject this_Player;
+    private Collider this_Collider;
+    private Rigidbody this_Rigidbody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this_Player = gameObject;
+        this_Collider = this_Player.GetComponent<Collider>();
+        this_Rigidbody = this_Player.GetComponent<Rigidbody>();
+    }
+
+    // Input
+    void PlayerInput()
+    {
+        // Temporary Vector 3 for input
+        Vector3 tempInput = new Vector3();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
