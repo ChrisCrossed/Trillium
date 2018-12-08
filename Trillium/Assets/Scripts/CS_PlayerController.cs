c2adfb541286da0e98c0a10704a949e4b54a2da6
Chris Christensen <Chriscrossed.86@gmail.com>
2018-12-08 00:26:53 -0800

    Player Controller / Player Input - Dec 8th, 2018
    - Created Input Manager (Core Tools - Player Input)
    - Created Player Controller (Player Controller)

--- Trillium/Assets/Scripts/CS_PlayerController.cs
+++ Trillium/Assets/Scripts/CS_PlayerController.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cs_PlayerController : MonoBehaviour
+public class CS_PlayerController : CT_PlayerInput
 {
     private GameObject this_Player;
     private Collider this_Collider;
     private Rigidbody this_Rigidbody;
 
     // Use this for initialization
-    void Start ()
+    new void Start ()
     {
         this_Player = gameObject;
         this_Collider = this_Player.GetComponent<Collider>();
         this_Rigidbody = this_Player.GetComponent<Rigidbody>();
+
+        // Run CT_PlayerInput's Start Function
+        base.Start();
     }
 
     // Input
@@ -22,12 +25,12 @@ public class cs_PlayerController : MonoBehaviour
         // Temporary Vector 3 for input
         Vector3 tempInput = new Vector3();
 
-
+        tempInput = base.GetInputVector();
     }
 	
 	// Update is called once per frame
-	void Update ()
+	new void Update ()
     {
-		
+        
 	}
 }
---
+++ Trillium/Assets/Scripts/CoreTools/CT_PlayerInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CT_PlayerInput : MonoBehaviour
+{
+    // Private Vectors
+    private Vector3 v3_InputVector;
+
+	// Use this for initialization
+	internal virtual void Start()
+    {
+        // Reset Vectors
+        v3_InputVector = new Vector3();
+	}
+
+    void EvaluateInputVector()
+    {
+        Vector3 tempVector = new Vector3();
+
+        // Evaluate input & assign to vector
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            tempVector.x = 1.0f;
+        else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+            tempVector.x = -1.0f;
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            tempVector.y = -1.0f;
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            tempVector.y = 1.0f;
+
+        // Normalize Vector
+        tempVector.Normalize();
+
+        // Assign temp vector to variable
+        v3_InputVector = tempVector;
+    }
+
+    internal Vector3 GetInputVector()
+    {
+        return v3_InputVector;
+    }
+	
+	// Update is called once per frame
+	internal virtual void Update()
+    {
+        EvaluateInputVector();
+	}
+}
